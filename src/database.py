8f5d2b71f36401628813cdd0033dc1b93dc9a549
Patryk Mroczyński <mroczynskip@gmail.com>
2018-12-07 18:32:56 +0100

    Implement functions for creating concrete databases

--- src/database.py
+++ src/database.py
@@ -5,7 +5,7 @@ POSTGRES_SYSTEM = 'postgresql'
 SQLITE_SYSTEM = 'sqlite'
 IN_MEMORY_SQLITE = 'memory'
 
-#TODO wsparcie dla MySQL
+#TODO support for MySQL
 
 def create_database(config: Dict[str, str]) -> pw.Database:
     try:
@@ -24,13 +24,23 @@ def create_database(config: Dict[str, str]) -> pw.Database:
 
     return db
 
-#TODO implement methods for concrete database management systems
-
 def _create_postgres_database(config: Dict[str, str]) -> pw.Database:
-    return pw.Database(None)
+    try:
+        return pw.PostgresqlDatabase(
+            database = config['Database'],
+            user = config.get('Username'),
+            password = config.get('Password'),
+            host = config.get('Host'),
+            port = int(config.get('Port')),
+        )
+    except KeyError:
+        raise KeyError('You need to specify database name in \'Database\' parameter')
 
 def _create_sqlite_database(config: Dict[str, str]) -> pw.Database:
-    return pw.Database(None)
+    try:
+        return pw.SqliteDatabase(config['Database'])
+    except KeyError:
+        raise KeyError('You need to specify database path in \'Database\' parameter')
 
 def _create_in_memory_database() -> pw.Database:
-    return pw.Database(None)
+    return pw.SqliteDatabase(':memory:')
