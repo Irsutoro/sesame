714129a5dfcf83cb53b576e4716d81aef73497a5
Patryk Mroczyński <mroczynskip@gmail.com>
2018-12-10 18:05:26 +0100

    Add User and Password models

--- src/models.py
+++ src/models.py
@@ -12,3 +12,15 @@ class BaseModel(pw.Model):
     @classmethod
     def models(cls) -> List[pw.Model]:
         return cls.__subclasses__()
+
+class User(BaseModel):
+    username = pw.FixedCharField(max_length=88) #base64 of sha512
+    password = pw.FixedCharField(max_length=88) #base64 of pbkdf2(sha512)
+    salt = pw.FixedCharField(max_length=44) #base64 of 32 byte salt
+    email = pw.CharField(max_length=254)
+    registration_date = pw.DateField()
+
+class Password(BaseModel):
+    user = pw.ForeignKeyField(model=User, backref='passwords')
+    block = pw.CharField(max_length=128) #part of password in base64 (max 96 character password)
+    label = pw.CharField(max_length=40) #user-defined identifier of site
