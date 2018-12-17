acd657e198cbeea5fd143c01e7ea1d4ec534f927
Patryk Mroczyński <mroczynskip@gmail.com>
2018-12-17 14:11:36 +0100

    Add type annotations

--- src/server.py
+++ src/server.py
@@ -2,39 +2,44 @@ import peewee as pw
 import hashlib
 import os
 from base64 import b64decode, b64encode
+from typing import List
 
 class Server:
 
     def __init__(self, database: pw.Database, base_model: pw.Model):
-        self.database = database
+        self.database = None
         self.base_model = base_model
-        self._initialize_proxy()
+        self.connect_to_database(database)
 
     def create_tables(self):
         self.database.create_tables(self.models())
 
-    def models(self):
+    def connect_to_database(self, database: pw.Database):
+        self.database = database
+        self._initialize_proxy()
+
+    def models(self) -> List[pw.Model]:
         return self.base_model.__subclasses__()
 
     def _initialize_proxy(self):
         self.base_model._meta.database.initialize(self.database)
 
     @staticmethod
-    def _generate_salt(length=32):
+    def _generate_salt(length: int) -> bytes:
         salt = os.urandom(length)
         return salt
 
     @staticmethod
-    def _hash_password(password, salt, iterations, algorithm='sha512'):
+    def _hash_password(password: str, salt: bytes, iterations: int, algorithm: str) -> bytes:
         hashed_password = hashlib.pbkdf2_hmac(algorithm, password.encode(), salt, iterations)
         return hashed_password
 
     @staticmethod
-    def _bytes_to_base(bytes):
-        base = b64encode(bytes).decode()
-        return base
+    def _bytes_to_base(value: bytes) -> str:
+        result = b64encode(value).decode()
+        return result
 
     @staticmethod
-    def _base_to_bytes(base):
-        bytes = b64decode(base)
-        return bytes
+    def _base_to_bytes(value: str) -> bytes:
+        result = b64decode(value)
+        return result
--- src/sesame.py
+++ src/sesame.py
@@ -3,15 +3,16 @@ from models import BaseModel, User
 import peewee as pw
 
 PBKDF2_ITERATIONS = 100000
+PBKDF2_ALGORITHM = 'sha512'
 
 class Sesame(Server):
     def __init__(self, database: pw.Database):
         super().__init__(database, BaseModel)
 
-    def register_user(self, username, password, email):
+    def register_user(self, username: str, password: str, email: str):
         salt = self._generate_salt(32)
         secret = self._generate_salt(32)
-        hashed_password = self._hash_password(password, salt, PBKDF2_ITERATIONS)
+        hashed_password = self._hash_password(password, salt, PBKDF2_ITERATIONS, PBKDF2_ALGORITHM)
 
         salt_in_base = self._bytes_to_base(salt)
         secret_in_base = self._bytes_to_base(secret)
